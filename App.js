092045edaac8e6f5f4848f5664b3c803dfb40b14
Adenilson-Silva-Dev <adenilsonrs3561@gmail.com>
2025-03-04 12:48:45 -0300

    Criacao arquivo styled-components

---
+++ App.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <Text>Dev Post d</Text>
+    </View>
+  )
+}
---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components/native";
+
+export const Container = styled.Container`
+flex: 1;
+float: inline-end;
+`
